60644caac932a194e6c803386bcc516a38fc151b
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-09-14 15:54:35 +0530

    get, post, put, delete checked using postman, sendFile method used

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello from get!')
+  res.sendFile('hello.html', { root: __dirname }  )
 })
 
 app.post('/items', (req, res) => {
   res.send('Hello from post!')
 })
 
-app.put('/', (req, res) => {
+app.put('/items/:id', (req, res) => {
   res.send('Hello from put!')
 })
 
-app.delete('/', (req, res) => {
+app.delete('/items/:id', (req, res) => {
   res.send('Hello from delete!')
 })
 
