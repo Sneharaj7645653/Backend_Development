be1190427bb6ddd79fbc1d39305f9951541ad7f4
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-09-14 15:25:26 +0530

    nodemon installed

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello from get!')
+})
+
+app.post('/items', (req, res) => {
+  res.send('Hello from post!')
+})
+
+app.put('/', (req, res) => {
+  res.send('Hello from put!')
+})
+
+app.delete('/', (req, res) => {
+  res.send('Hello from delete!')
 })
 
 app.listen(port, () => {
